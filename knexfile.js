d71327086332a0b1905dcf1bcd310fe5bb317cc4
Michael Kastner <kastner@galt.de>
2021-01-28 06:03:20 +0100

    had to move knexfile.js to project root

---
+++ knexfile.js
@@ -0,0 +1,26 @@
+// knexfile.js
+// must be in root of project
+const EnvVars = require('mk-env-vars');
+const envVars = EnvVars({deploy: 'TEST'});
+
+module.exports = {
+  client: 'mysql',
+  connection: {
+    host     : envVars('MYSQLHOST'),
+    user     : envVars('DBUSER'),
+    password : envVars('DBPASSWORD'),
+    database : 'mk_base_adapter_test',
+    charset  : 'utf8',
+    preciseTimestamps: true
+  },
+  debug: false,
+  pool: {
+    min: 0,
+    max: 30 
+  },
+  migrations: {
+    directory: './test/bookshelf/migrations',
+    tableName: 'knex_migrations'
+  }
+};
+
